eeae086e473b62c4bb05ea0c9fcb37616310fd9b
Sobhan Moradi <sobhan.mrds@gmail.com>
2024-03-05 21:23:28 +0330

    some little change

--- config/settings.py
+++ config/settings.py
@@ -118,9 +118,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'fa'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'GMT'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -130,8 +130,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL ='/media/'
+STATIC_URL = 'static/'
+MEDIA_URL ='media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- root/views/HomeView.py
+++ root/views/HomeView.py
@@ -1,4 +1,4 @@
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'root/index.html'
